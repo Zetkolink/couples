4f004894f814d4d35f5c8b994cf4360a9a486231
Zakir <z.chalimov@yandex.ru>
2020-11-22 17:10:04 +0300

    Updates

---
+++ precache-manifest.d275354ea3645596a26257b263216c8d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d2439f0a1ed0cadcd652fbe7b2125b",
+    "url": "/couples/index.html"
+  },
+  {
+    "revision": "be782ba7dd702742beb8",
+    "url": "/couples/static/css/2.8cdfe4cf.chunk.css"
+  },
+  {
+    "revision": "a49f49da9d6d3fb45924",
+    "url": "/couples/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "be782ba7dd702742beb8",
+    "url": "/couples/static/js/2.cd1e86d1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/couples/static/js/2.cd1e86d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a49f49da9d6d3fb45924",
+    "url": "/couples/static/js/main.019a375b.chunk.js"
+  },
+  {
+    "revision": "915e7bee32d5471e4639",
+    "url": "/couples/static/js/runtime-main.92cf7283.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/couples/static/media/persik.4e1ec840.png"
+  }
+]);
